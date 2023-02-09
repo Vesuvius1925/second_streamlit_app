2642f5e820dc1317087f96026b79f35388aabde1
Vesuvius1925 <124305351+Vesuvius1925@users.noreply.github.com>
2023-02-10 06:23:34 +0800

    Update second_streamlit_app.py

--- second_streamlit_app.py
+++ second_streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
